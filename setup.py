a70b5f11ca91fe95025c35e083f02e2633de3c3c
george <george>
2019-09-24 18:55:50 -0400

    clean code

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="cascade",
-    version="0.1",
+    version="0.1.0",
     scripts=['cascade'],
     install_requires=['numpy', 'scikit-rf'],
 )
